06bfff7df8fbab06db57d179c922aca8a0919e3c
samuel-angel <pujazonlamashectorfernando@gmail.com>
2022-01-13 23:20:59 -0500

    finalizado tarea JSV(con correciones)

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -27,9 +27,9 @@ function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
-  /*Constructor.prototype.saludar()=function(){
+  Constructor.prototype.saludar=function(){
     return "Hello World!";
-  }*/
+  }
 }
 
 function agregarStringInvertida() {
